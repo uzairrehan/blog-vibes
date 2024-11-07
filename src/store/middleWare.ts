a5cc995be8633d1c69aec59a63586b847de1b3df
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-11-07 22:27:01 +0500

    update

--- src/store/middleWare.ts
+++ src/store/middleWare.ts
@@ -1,8 +1,16 @@
 import { Middleware } from '@reduxjs/toolkit';
 import { setUser } from "@/store/lib/slices/userSlice";
+import { User } from 'firebase/auth';
 
-const localStorageMiddleware: Middleware = (store) => (next) => (action) => {
-  if (action.type === setUser.type) {
+// Type guard to check if the action is of type SetUserAction
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isSetUserAction = (action: any): action is { payload: User; type: "user/setUser" } => {
+  return action.type === setUser.type;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const localStorageMiddleware: Middleware = (store) => (next) => (action: unknown) => {
+  if (isSetUserAction(action)) {
     localStorage.setItem('user', JSON.stringify(action.payload));
   }
   return next(action);
