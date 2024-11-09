3983e79e6f4c76e7d0d969e36cfa6af40e29da04
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-11-09 11:44:24 +0500

    verification complete

--- src/app/authenticate/verify/page.tsx
+++ src/app/authenticate/verify/page.tsx
@@ -1,43 +1,52 @@
 "use client";
 
+import Loading from "@/components/loading";
 import { auth } from "@/firebase/firebaseconfig";
-import { sendEmailVerification } from "firebase/auth";
+import { sendEmailVerification, User } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 const EmailVerification = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const email = auth.currentUser?.email;
-  const currentUser = auth.currentUser;
+  const [email, setEmail] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(null);
   const route = useRouter();
 
-  function checkUser() {
-    if (currentUser?.emailVerified) {
-      route.push("/");
-      return;
-    }
-
-    if (!currentUser?.emailVerified && currentUser) {
-      try {
-        sendEmailVerification(currentUser);
-        toast.success(`Verification email sent to ${email}`);
-        setIsLoading(false);
-      } catch (e) {
-        toast.error(`failed ${e}`);
-        setIsLoading(false);
-      }
-    }
-  }
-
   useEffect(() => {
-    checkUser();
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      setEmail(currentUser.email);
+      setUser(currentUser);
+      if (currentUser.emailVerified) {
+        route.push("/");
+      } else {
+        sendVerificationEmail(currentUser);
+      }
+    } else {
+      setIsLoading(false);
+    }
   }, []);
 
-  const handleResend = async () => {
-    setIsLoading(true);
-    checkUser();
+  const sendVerificationEmail = async (currentUser: User) => {
+    try {
+      await sendEmailVerification(currentUser);
+      toast.success(`Verification email sent to ${currentUser.email}`);
+      setIsLoading(false);
+    } catch (e) {
+      toast.error(`Failed to send verification email: ${e}`);
+      setIsLoading(false);
+    }
   };
+
+  const handleResend = async () => {
+    if (user) {
+      setIsLoading(true);
+      sendVerificationEmail(user);
+    }
+  };
+
+  if (isLoading) return <Loading />;
 
   return (
     <div className="min-h-full flex items-center justify-center">
@@ -47,16 +56,19 @@ const EmailVerification = () => {
         </h2>
 
         <div className="space-y-4">
-          <div>
-            Check your email! <br /> {email?.toString()}
-          </div>
+          {email && (
+            <div>
+              Check your email! {email.toString()} <br />
+              Verify it and then reload this page!
+            </div>
+          )}
 
           <button
-            className={`btn btn-primary w-full`}
+            className={`btn btn-primary w-full ${isLoading ? "loading" : ""}`}
             onClick={handleResend}
             disabled={isLoading}
           >
-            {isLoading ? "Sending..." : "Send Again"}
+            {isLoading ? "Sending..." : "Resend"}
           </button>
         </div>
       </div>
