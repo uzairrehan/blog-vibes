aa441c31f07ddc3703a5aa8d0595ff5a2efdb4bc
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-11-20 00:40:50 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 "use client";
-import AllCards from "@/components/allCards";
-import Footer from "@/components/footer";
 import { db } from "@/firebase/firebaseconfig";
 import { CardData } from "@/types/types";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import AllCards from "@/components/allCards";
+import Footer from "@/components/footer";
 
 export default function Home() {
   const [allCards, setAllCards] = useState<CardData[]>([]);
 
   async function getData() {
-    const querySnapshot = await getDocs(collection(db, "blogs"));
+    const q = query(collection(db, "blogs"),  where("deleted", "!=", true))
+    const querySnapshot = await getDocs(q);
     const dataArray: CardData[] = [];
     querySnapshot.forEach((doc) => {
       dataArray.push(doc.data() as CardData);
